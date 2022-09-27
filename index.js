1a0ddd2cf1373a1a5059604d775a493c566ddfa9
すずねーう <suzuneu@suzuneu.com>
2022-09-27 07:47:33 +0000

    45

--- index.js
+++ index.js
@@ -23,7 +23,12 @@ client.on("ready", () => {
 client.on("interactionCreate", (i) => {
     if (!i.isChatInputCommand()) return;
     const v = i.options.getInteger("wave");
-    i.reply(`${eval(`4*${v}-109-(-1)**${v}`)}`);
+    if (v > 45) {
+        i.reply(`${eval(`4*${v}-109-(-1)**${v}`)}`);
+    } else {
+        const p = Math.floor(v / 5);
+        i.reply(`${(p + (v % 5)) + (v % 5 >= 3 ? 1:0) + (v % 5 === 0 ? 2:0)}`);
+    }
 });
 
 client.login(process.env.TOKEN);
